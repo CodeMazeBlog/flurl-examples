81ff8c527be56df4087c906dc292554ebc615e28
Vladimir Pecanac <v.pecanac@vegaitsourcing.rs>
2018-09-07 16:23:05 +0200

    Added unit test project

---
+++ FlurlTests/Tests.cs
@@ -0,0 +1,26 @@
+using System.Net.Http;
+using Flurl.Http.Testing;
+using FlurlExamples;
+using FlurlExamples.Constants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlurlTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void GetRepositories_ShouldHaveBeenCalled_AtLeastOnce()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                var flurlRequestHandler = new FlurlRequestHandler();
+                var result = flurlRequestHandler.GetRepositories().Result;
+
+                httpTest.ShouldHaveCalled(RequestConstants.BaseUrl + "user" + "/repos")
+                    .WithVerb(HttpMethod.Get)
+                    .Times(1);
+            }
+        }
+    }
+}
